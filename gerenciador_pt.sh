faafedfa45f22f116fc59bfe15755806318a2fb7
Deco Belli <andreanimado@gmail.com>
2025-05-13 22:38:30 -0300

    Token

--- gerenciador_pt.sh
+++ gerenciador_pt.sh
@@ -50,7 +50,7 @@ WHITELIST_IPS=("166.0.189.163")
 VALIDATED=false
 # === CONFIGURAÇÕES DE VERSÃO ===
 VERSAO_LOCAL="1.0.1"  # Versão atual do script
-URL_SCRIPT="https://raw.githubusercontent.com/d-belli/Multi_Bot-Plano01/refs/heads/main/gerenciador_pt.sh"  # Link para o conteúdo do script no GitHub
+URL_SCRIPT="https://raw.githubusercontent.com/d-belli/Multi_Bot-Plano01/refs/heads/main/gerenciador_pt.sh?token=GHSAT0AAAAAADCRJYHQQTRJBXIP2QZR4WHA2BD6Q7Q"  # Link para o conteúdo do script no GitHub
 
 # Obtém o nome do script atual (ex.: gerenciador.sh)
 SCRIPT_NOME=$(basename "$0")
