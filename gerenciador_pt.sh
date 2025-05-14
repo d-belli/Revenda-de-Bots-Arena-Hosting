20af161d187e60a0568ddb1ab24fe8ea60a51c02
Deco Belli <andreanimado@gmail.com>
2025-05-13 23:10:34 -0300

    Att

--- gerenciador_pt.sh
+++ gerenciador_pt.sh
@@ -49,7 +49,7 @@ WHITELIST_HOSTNAMES=("arenahosting.com.br")
 WHITELIST_IPS=("166.0.189.163")
 VALIDATED=false
 # === CONFIGURAÇÕES DE VERSÃO ===
-VERSAO_LOCAL="1.0.3"  # Versão atual do script
+VERSAO_LOCAL="1.0.4"  # Versão atual do script
 URL_SCRIPT="https://raw.githubusercontent.com/d-belli/Multi_Bot-Plano01/refs/heads/main/gerenciador_pt.sh"  # Link para o conteúdo do script no GitHub
 
 # Obtém o nome do script atual (ex.: gerenciador.sh)
