477c0da3116326f7dc35f1f31a7dbe5d6a147367
Deco Belli <andreanimado@gmail.com>
2025-05-14 19:35:48 -0300

    Vaidação

--- gerenciador.sh
+++ gerenciador.sh
@@ -18,25 +18,6 @@ ARROW='➡️'
 CIRCLE_ON='◉'  # Círculo verde para ON
 CIRCLE_OFF='○' # Círculo vazio para OFF
 
-# === CONFIGURACIÓN ===
-# Configuración de la API esperada. Esta es la clave utilizada para validar la conexión con el gestor principal.
-# API_ESPERADA="ARENAHOSTING"
-
-# === FUNCIÓN PARA VALIDAR LA API ===
-# Esta función verifica si la API proporcionada por el gestor principal corresponde con la clave esperada.
-# Si no coincide, el script muestra un mensaje de error y se cierra.
-# El usuario no debe modificar esta función a menos que comprenda cómo funciona la validación.
-# validar_api() {
-#     API_RECIBIDA=$1
-#     if [ "$API_RECIBIDA" != "$API_ESPERADA" ]; then
-#         echo -e "${RED}LA API NO PUEDE CONECTARSE AL ARCHIVO MANAGER.SH.${NC}"
-#         echo -e "${YELLOW}POR FAVOR, PROPORCIONE EL ARCHIVO DE CONFIGURACIÓN PARA EJECUTAR ESTE ARCHIVO.${NC}"
-#         echo -e "${YELLOW}SI NO SABE, CONTACTE CON NUESTRO SOPORTE:${NC}"
-#         echo -e "${CYAN}https://arenahosting.com.br${NC}"
-#         exit 1
-#     fi
-# }
-
 # === CONFIGURAÇÕES PRINCIPAIS ===
 BASE_DIR="/home/container"  # Diretório base onde os ambientes serão criados.
 NUM_AMBIENTES=5 # Número de ambientes que serão configurados.
@@ -47,9 +28,9 @@ BACKUP_NUM_AMBIENTES="${BASE_DIR}/num_ambientes_backup.txt"  # Arquivo para back
 # === CONFIGURAÇÕES ===
 WHITELIST_HOSTNAMES=("arenahosting.com.br")
 WHITELIST_IPS=("166.0.189.163")
-VALIDATED=false
+VALIDATED=true
 # === CONFIGURAÇÕES DE VERSÃO ===
-VERSAO_LOCAL="1.0.5"  # Versão atual do script
+VERSAO_LOCAL="1.0.6"  # Versão atual do script
 URL_SCRIPT="https://raw.githubusercontent.com/d-belli/Multi_Bot-Plano01/refs/heads/main/gerenciador_pt.sh"  # Link para o conteúdo do script no GitHub
 
 # Obtém o nome do script atual (ex.: gerenciador.sh)
